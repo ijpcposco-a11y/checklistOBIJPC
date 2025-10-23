3fab5507b464a3f3198d7f19aa37a872983145da
ijpcposco-a11y <ijpcposco@gmail.com>
2025-10-23 11:44:05 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Checklist OB Kantor',
+      title: 'Checklist OB IJPC',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
       home: FutureBuilder<bool>(
