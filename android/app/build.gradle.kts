d345f9b14781bf55d7e402732866dc4ce0f517e5
ijpcposco-a11y <ijpcposco@gmail.com>
2025-10-24 07:44:41 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         //minSdk = flutter.minSdkVersion
         //targetSdk = flutter.targetSdkVersion
-        minSdkVersion(23)
+        minSdkVersion(24)
         targetSdkVersion(33)
         versionCode = flutter.versionCode
         versionName = flutter.versionName
